35008a6ab390f4f1750a7fd709158e0ab0d2eb20
Shreyas <shreyaskowshik@gmail.com>
2024-08-03 21:42:56 +0530

    Add HW0

--- hw-intro/limits.c
+++ hw-intro/limits.c
@@ -3,8 +3,32 @@
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    rlim_t stack_size;
+    rlim_t process_limit;
+    rlim_t max_fd;
+
+    int status = getrlimit(RLIMIT_STACK, &lim);
+    if(status == 0) stack_size = lim.rlim_cur;
+    else {
+        printf("Stack size could not be obtained\n");
+        return 0;
+    }
+    status = getrlimit(RLIMIT_NPROC, &lim);
+    if(status == 0) process_limit = lim.rlim_cur;
+    else {
+        printf("Process_limit could not be obtained\n");
+        return 0;
+    }
+    status = getrlimit(RLIMIT_NOFILE, &lim);
+    if(status == 0) max_fd = lim.rlim_cur;
+    else {
+        printf("Max fd could not be obtained\n");
+        return 0;
+    }
+
+
+    printf("stack size: %ld\n", stack_size);
+    printf("process limit: %ld\n", process_limit);
+    printf("max file descriptors: %ld\n", max_fd);
     return 0;
 }
